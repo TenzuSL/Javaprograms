a09f8a01c7a78a6bdaf347af6a924eb5ae9a0d85
TenzuSL <shivshan.klu@gmail.com>
2021-10-01 23:27:20 +0200

    Last program

---
+++ learnLoops.java
@@ -0,0 +1,27 @@
+package Javabasics;
+
+public class learnLoops {
+	
+	static int x =12;
+	static int y =10;
+
+	public static void main(String[] args) {
+		
+		
+//		while(x > 5)
+//		{
+//			System.out.println(x);
+//			x--;
+//		}
+//		
+		
+		do
+		{
+			System.out.println("print"+x);
+			x--;
+		}while( x >= y );
+			
+
+	}
+
+}
